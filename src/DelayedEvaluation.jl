51a4d993b3477ca901202b76ed7f6ea6093f75f6
Felippe Alves <alvespef@aston.ac.uk>
2022-10-26 20:31:35 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/DelayedEvaluation.jl
@@ -0,0 +1,5 @@
+module DelayedEvaluation
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DelayedEvaluation
+using Test
+
+@testset "DelayedEvaluation.jl" begin
+    # Write your tests here.
+end
